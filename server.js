652df59c7a1b02ec11b40f9c4456e1726cbfa7f4
Brenden Smith <brendthebean@gmail.com>
2023-04-20 10:51:36 -0500

    products fetch setup

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const apiRouter = require('./api')
 const app = express();
 const PORT = 3000;
-const path = require("path")
+const path = require("path");
 const { client } = require('./DB/index');
 
 app.use("/dist", express.static(path.join(__dirname, 'dist')));
