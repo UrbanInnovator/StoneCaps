6a62605c1e9d621784d7d48c57d12bb18336b395
Brenden Smith <brendthebean@gmail.com>
2023-04-26 10:31:28 -0500

    pulling

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 
+//middleware to insure the token is being passed approriatly
 router.use(async (req, res, next) => {
     const prefix = 'Bearer ';
     const auth = req.header('Authorization');
