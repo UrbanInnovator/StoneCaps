696455a25f39041647500c9d713965664b285775
landen <landenjwilliams@gmail.com>
2023-04-27 11:09:39 -0500

    order api routes

--- api/Order.js
+++ api/Order.js
@@ -1,2 +1,38 @@
 const express = require('express');
 const router = express.Router();
+
+const { 
+    createOrder,
+    getAllOrdersByUserId
+
+} = require('../DB/orders.js');
+
+const {
+    getAllOrderItemsByOrderId
+} = require('../DB/orderItems.js')
+
+// GET /api/order
+router.post ('/', async (req, res, next) => {
+    try {
+        const user = await req.user;
+        const orders = await getAllOrdersByUserId(user.id);
+        res.send(orders);
+
+    } catch (error) {
+        next({ error: error});
+    }
+})
+
+// POST /api/order
+
+router.post ('/', async (req, res, next) => {
+    try {
+        const user = await req.user;
+        const newOrder = await createOrder(user.id);
+        res.send(newOrder);
+        
+    } catch (error) {
+        next({ error: error});
+    }
+})
+
