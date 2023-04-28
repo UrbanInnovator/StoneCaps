61f4e923ac6e6ad913938bf124410ca892050d51
landen <landenjwilliams@gmail.com>
2023-04-28 15:54:50 -0500

    oops missed some syntax

--- api/Order.js
+++ api/Order.js
@@ -24,9 +24,10 @@ router.post ('/', async (req, res, next) => {
 })
 
 // GET /api/order/:orderId
-router.post ('/', async (req, res, next) => {
+router.post ('/:orderId', async (req, res, next) => {
+    const { orderId } = req.params;
+
     try {
-        const { orderId } = req.params;
         const order = await getOrderById(orderId);
         res.send(order);
 
